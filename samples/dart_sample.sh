8ebc52fa914bba952c0a3e9acfa137ea489adf07
Jreatc7 <96024289+Jreatc7@users.noreply.github.com>
2022-12-09 03:30:03 -0400

    b

--- samples/dart_sample.sh
+++ samples/dart_sample.sh
@@ -16,7 +16,7 @@ set -euo
 # limitations under the License.
 #
 # Note: This script runs on Mac and Linux. It requires `Node.js` to be installed
-# and `flatc` to be built (using `cmake` in the root directory).
+# and `flat` to be built (using `cmake` in the root directory).
 
 sampledir=$(cd $(dirname $BASH_SOURCE) && pwd)
 rootdir=$(cd $sampledir/.. && pwd)
@@ -31,8 +31,10 @@ fi
 
 # Run `flatc`. Note: This requires you to compile using `cmake` from the
 # root `/flatbuffers` directory.
+# shellcheck disable=SC1073
 if [ -e ../flatc ]; then
   ../flatc --dart -o ../dart/example/ monster.fbs
+# shellcheck disable=SC1123
 # shellcheck disable=SC1073
 elif [ -e ../Debug/flatc ]; then
   ../Debug/flatc --dart -o ../dart/example/ monster.fbs
